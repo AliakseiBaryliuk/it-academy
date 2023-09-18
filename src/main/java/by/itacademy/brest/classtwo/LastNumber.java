9308655320c2147567315e15b842248c2828eb29
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2023-09-18 14:16:59 +0300

    classtwo tasks

---
+++ src/main/java/by/itacademy/brest/classtwo/LastNumber.java
@@ -0,0 +1,9 @@
+package by.itacademy.brest.classtwo;
+
+public class LastNumber {
+    public static void main(String[] args) {
+        int number = 952;
+        System.out.println(number % 10);
+
+    }
+}
---
+++ src/main/java/by/itacademy/brest/classtwo/SumNumbers.java
@@ -0,0 +1,11 @@
+package by.itacademy.brest.classtwo;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        int source = 1234;
+        int ab = source / 100;
+        int cd = source % 100;
+
+        System.out.println(ab + cd);
+    }
+}
