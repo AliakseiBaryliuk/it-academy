9cc1f44fd75cf839247decbde7351150fcc53a64
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2023-09-26 15:41:39 +0300

    circle

--- src/main/java/by/itacademy/brest/class2/ByteProblem.java
+++ src/main/java/by/itacademy/brest/class2/ByteProblem.java
@@ -2,26 +2,26 @@ package by.itacademy.brest.class2;
 
 public class ByteProblem {
     public static void main(String[] args) {
-//        for (byte i = 1; i <= 200; i++) {
-//            if (i == 127) {
-//                System.out.println("circle");
-//            }
-//            System.out.println(i);
-//        }
-////        byte a = (byte) 129;
-////        System.out.println("circle: " + a);
-        int originalNumber = 10; // Начальное число
-        int numberOfShifts = 4;  // Количество сдвигов влево
-
-// Выполняем сдвиг влево
-        int shiftedNumber = originalNumber << numberOfShifts;
-
-// Теперь выполняем обратное смещение вправо
-        int previousNumber = shiftedNumber >> numberOfShifts;
-
-        System.out.println("Исходное число: " + originalNumber);
-        System.out.println("Число после сдвига влево: " + shiftedNumber);
-        System.out.println("Предыдущее число: " + previousNumber);
+        for (byte i = 1; i <= 128; i++) {
+            if (i == 127) {
+                System.out.println("circle");
+            }
+            System.out.println(i);
+        }
+//        byte a = (byte) 129;
+//        System.out.println("circle: " + a);
+//        int originalNumber = 10; // Начальное число
+//        int numberOfShifts = 4;  // Количество сдвигов влево
+//
+//// Выполняем сдвиг влево
+//        int shiftedNumber = originalNumber << numberOfShifts;
+//
+//// Теперь выполняем обратное смещение вправо
+//        int previousNumber = shiftedNumber >> numberOfShifts;
+//
+//        System.out.println("Исходное число: " + originalNumber);
+//        System.out.println("Число после сдвига влево: " + shiftedNumber);
+//        System.out.println("Предыдущее число: " + previousNumber);
 
     }
 
