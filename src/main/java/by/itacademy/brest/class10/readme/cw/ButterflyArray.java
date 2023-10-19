9eb1087c47ca820ac2dd80a5aa75d86e33e4cdfb
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2023-10-19 16:18:07 +0300

    ButterflyArray

--- src/main/java/by/itacademy/brest/class10/readme/cw/ButterflyArray.java
+++ src/main/java/by/itacademy/brest/class10/readme/cw/ButterflyArray.java
@@ -2,7 +2,7 @@ package by.itacademy.brest.class10.readme.cw;
 
 public class ButterflyArray {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
         int[][] butterfly = new int[n][n];
 
         for (int i = 0; i < n; i++) {
